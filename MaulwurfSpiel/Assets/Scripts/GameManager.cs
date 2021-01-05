6dad8c510f6852aa5a8c268578bad532be620cba
TomEckert00 <toe00@web.de>
2021-01-05 13:36:31 +0100

    init + basic setup

---
+++ MaulwurfSpiel/Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Button[] gameFields;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI timeText;
+    private int score;
+    private int highscore;
+    private Time time;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
